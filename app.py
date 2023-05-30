1108738eb80fd163fb6fa4ca58cabd7760f408d2
Jamie Badley <111872934+jamesbadley96@users.noreply.github.com>
2023-05-30 11:36:16 +0100

    Update app.py full

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import logging
@@ -7,8 +8,11 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 app = Flask(__name__)
 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 sp_oauth = SpotifyOAuth(client_id="5ef3272dcd5e49598a655deb26b81aeb",
-                        client_secret="f6b712b3f7c1407396dc3cc2ed9f0e5f",                                            
+                        client_secret="f6b712b3f7c1407396dc3cc2ed9f0e5f",
                         redirect_uri="http://localhost:8080/callback"
                         )
 
@@ -16,18 +20,26 @@ sp_oauth = SpotifyOAuth(client_id="5ef3272dcd5e49598a655deb26b81aeb",
 def callback():
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
-    access_token = token_info['access_token']                            
+    access_token = token_info['access_token']
 
-    # Create Spotify client with this token
-    sp = spotipy.Spotify(auth_manager=sp_oauth)
-
-    # Maybe save this Spotify client to a database or session?
-    # This part depends on the rest of your application
+    # Save the access_token to the session
+    session['access_token'] = access_token
 
     return "Callback done"  # Or however you want to handle this route
 
 def get_track_id(track_name, artist_name):
     logging.info('Getting track ID for track "%s" by artist "%s"', track_name, artist_name)
+
+    # Retrieve the access_token from the session
+    access_token = session.get('access_token', None)
+
+    if access_token is None:
+        logging.error("Access token not found in session.")
+        return None
+
+    # Create a new Spotify client with the access token
+    sp = spotipy.Spotify(auth=access_token)
+
     results = sp.search(q=f'track:{track_name} artist:{artist_name}', type='track')
     items = results['tracks']['items']
     if not items:
@@ -35,10 +47,21 @@ def get_track_id(track_name, artist_name):
         return None
     else:
         logging.info('Found track ID: %s', items[0]['id'])
-        return items[0]['id']      
+        return items[0]['id']
 
 def get_recommendations(track_id):
     logging.info('Getting recommendations for track ID: %s', track_id)
+
+    # Retrieve the access_token from the session
+    access_token = session.get('access_token', None)
+
+    if access_token is None:
+        logging.error("Access token not found in session.")
+        return None
+
+    # Create a new Spotify client with the access token
+    sp = spotipy.Spotify(auth=access_token)
+
     if track_id is None:
         logging.warning("Track ID is None. Can't get recommendations.")
         return None
@@ -50,7 +73,6 @@ def get_recommendations(track_id):
         track_info.append({'name': track['name'], 'artists': artist_names, 'genres': genres})
     logging.info('Fetched recommendations successfully.')
     return track_info
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
